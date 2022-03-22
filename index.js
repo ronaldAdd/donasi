e1acca1aaff3d187f0470d736a515e2d9d4821f9
ronald <sschouse@gmail.com>
2022-03-22 23:16:14 +0700

    port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express =  require('express')
 const app = express();
-const port = 5000
+const port = process.env.PORT || 5000
 const multer = require('multer');
 const bodyParser = require('body-parser');
 const donationSchema = require('./models/donation');
